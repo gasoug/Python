68bcc29257a7f356966b3a366b96cc9d15356bf8
gasoug <gabriel.sousago@gmail.com>
2022-08-19 14:22:01 -0300

    Adcionado controle de Status

--- Email Attachments/README.md
+++ Email Attachments/README.md
@@ -3,3 +3,5 @@
 - Há a possibilidade de usar o mesmo código para o Excel, por exemplo, alterando o tipo de arquivo e biblioteca importada;
 - Dentro do código está explicando todos os campos que será preciso alteração;
 - Referente a senha, é a senha que é gerado no google, acessando a conta, clicando na aba "Segurança" e descendo até a parte de "Senhas de APP" e realizar o processo que gera essa senha;
+- Há um campo de "controle" para não enviar a mesma fatura 2 vezes, que seria o "Status", estando diferente de "Enviado", nesse exemplo, o boleto será enviado. Para a não duplicicade, basta apenas preencher na coluna como "Enviado";
+- É necessário que tanto na tabela quanto no arquivo do anexo, o nome seja exatamente o mesmo, caso contrário, não irá encontrar o arquivo para anexar;
--- Email Attachments/email-send.py
+++ Email Attachments/email-send.py
@@ -6,56 +6,60 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
-#VAI COLOCAR O NOME DO ARQUIVO CSV ONDE FARA O LOOP
-with open('Clientes.csv', encoding='utf-8') as list:
-    new_list = csv.reader(list, delimiter=';')
-    for i in new_list:
-        #A ORDEM QUE ESTA NO CSV
-        nome = i[0]
-        contato = i[1]
-        status = i[2]
-        body = """
-            <p>Olá</p>
-            <p>Segue em anexo o arquivo em pdf conforme solicitado.</p>
-            """
-        #VAI COLOCAR O EMAIL DO QUAL SERÁ ENVIADO OS ARQUIVOS(SEU EMAIL)    
-        sender = 'XXXXXXXXX'
-        #VAI COLOCAR A SENHA QUE VOCÊ GERA PELAS CONFIGURAÇÕES DO GOOGLE, REFERENTE A 'SENHAS DE APP'
-        password = 'XXXXXXXXX'
-        receiver = f'{contato}'
-        
-        #informações de envio
-        message = MIMEMultipart()
-        message['From'] = sender
-        message['To'] = receiver
-        message['Subject'] = 'Assunto do email'
-        
-        message.attach(MIMEText(body, 'html'))
-        # O ARQUIVO PARA ANEXO, DEVE ESTAR NA MESMA PASTA ONDE ESTÁ O CÓDIGO
-        pdfname = f'{nome}.pdf'
-        # abrir o arquivo em modo binario
-        binary_pdf = open(pdfname, 'rb')
-        
-        payload = MIMEBase('application', 'octate-stream', Name=pdfname)
-        payload.set_payload((binary_pdf).read())
-        
-        # Passar para a base64
-        encoders.encode_base64(payload)
-        
-        # abrir o header com o arquivo pdf
-        payload.add_header('Content-Decomposition', 'attachment', filename=pdfname)
-        message.attach(payload)
-        
-        #porta do gmail
-        session = smtplib.SMTP('smtp.gmail.com', 587)
-        
-        #habilitar segurança
-        session.starttls()
-        
-        #login com a senha
-        session.login(sender, password)
-        
-        text = message.as_string()
-        session.sendmail(sender, receiver, text)
-        session.quit()
-        print('Email Enviado!')
+#VAI COLOCAR O NOME DO ARQUIVO CSV QUE FARA O LOOP
+def send():
+    with open('Clientes.csv', encoding='utf-8') as list:
+        new_list = csv.reader(list, delimiter=';')
+        for i in new_list:
+            #A ORDEM DOS CAMPOS QUE ESTA NO CSV
+            nome = i[0]
+            contato = i[1]
+            status = i[2]
+            if status != 'Enviado':
+                body = """
+                    <p>Olá</p>
+                    <p>Segue em anexo o boleto conforme solicitado.</p>
+                    """
+                #VAI COLOCAR O EMAIL DO QUAL SERÁ ENVIADO OS ARQUIVOS(SEU EMAIL)    
+                sender = 'XXXXXXX'
+                #VAI COLOCAR A SENHA QUE VOCÊ GERA PELAS CONFIGURAÇÕES DO GOOGLE, REFERENTE A 'SENHAS DE APP'
+                password = 'XXXXXXXX'
+                receiver = f'{contato}'
+                
+                #informações de envio
+                message = MIMEMultipart()
+                message['From'] = sender
+                message['To'] = receiver
+                message['Subject'] = 'Assunto do email'
+                
+                message.attach(MIMEText(body, 'html'))
+                #NA PARTE ONDE TA ESCRITO 'BOLETO', VOCÊ VAI COLOCAR A PASTA QUE TA COLOCANDO O ARQUIVO, DEVE SER A MESMA DOS ARQUIVOS EM PDF
+                #pdfname = away + '\\Boleto\\' + f'{Nome}.pdf'
+                pdfname = f'{nome}.pdf'
+                # abrir o arquivo em modo binario
+                binary_pdf = open(pdfname, 'rb')
+                
+                payload = MIMEBase('application', 'octate-stream', Name=pdfname)
+                payload.set_payload((binary_pdf).read())
+                
+                # Passar para a base64
+                encoders.encode_base64(payload)
+                
+                # abrir o header com o arquivo pdf
+                payload.add_header('Content-Decomposition', 'attachment', filename=pdfname)
+                message.attach(payload)
+                
+                #porta do gmail
+                session = smtplib.SMTP('smtp.gmail.com', 587)
+                
+                #habilitar segurança
+                session.starttls()
+                
+                #login com a senha
+                session.login(sender, password)
+                
+                text = message.as_string()
+                session.sendmail(sender, receiver, text)
+                session.quit()
+                print('Email Enviado!')
+send()            
